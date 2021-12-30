673ebd9fef6753980d24ecb2de0bff8030acd874
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-12-29 21:28:19 -0800

    create slice

---
+++ src/state/slice/carouselSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CarouselSliceState {
+  currentImageIndex: number;
+  numImages: number;
+}
+
+const initialState: CarouselSliceState = {
+  currentImageIndex: -1, // -1 represent initially don't show any
+  numImages: 8,
+};
+
+export const carouselSlice = createSlice({
+  name: "carousel",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      if (state.currentImageIndex > state.numImages) {
+        state.currentImageIndex = 0;
+      } else {
+        state.currentImageIndex++;
+      }
+    },
+  },
+});
+
+export const { increment } = carouselSlice.actions;
+
+export default carouselSlice.reducer;
